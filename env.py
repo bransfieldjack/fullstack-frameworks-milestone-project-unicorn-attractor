f5a52a63c42cddbf6c24cd7ab11b88331b377146
Jack Bransfield <bransfieldjack@gmail.com>
2019-01-06 05:19:00 +0000

    Setting up stripe integration.

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_IPY2fGRoZkPTnPvrX1YC8WDH')
+os.environ.setdefault('STRIPE_SECRET', 'sk_test_ek41IN8TXXihmkgdq16PinDn')
+
+
--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -132,13 +132,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_PORT = 587
 
