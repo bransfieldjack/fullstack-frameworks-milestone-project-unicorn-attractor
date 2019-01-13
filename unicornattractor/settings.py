05af69816ad52d011cb23f867dc1a773e95f9a06
Jack Bransfield <bransfieldjack@gmail.com>
2019-01-13 01:27:55 +0000

    Deleted env.py, adding again.

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['unicorn-attractor-jackalack117.c9users.io',
                 'unicorn-attractor-ci.herokuapp.com']
